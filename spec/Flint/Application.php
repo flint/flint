e4f4666ab2e632fbf6e032726d08dcd67eb9481b
Henrik Bjørnskov <henrik@bjrnskov.dk>
2013-01-09 11:57:35 +0100

    add simple spec

---
+++ spec/Flint/Application.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace spec\Flint;
+
+use PHPSpec2\ObjectBehavior;
+
+class Application extends ObjectBehavior
+{
+    function let()
+    {
+        $this->beConstructedWith(sys_get_temp_dir());
+    }
+
+    function it_defaults_to_false_for_debug()
+    {
+        $this->raw('debug')->shouldReturn(false);
+        $this->raw('debug');
+    }
+
+    function it_saves_the_root_dir_and_debug_on_container()
+    {
+        $this->beConstructedWith('/My/Custom/Dir', true);
+
+        $this->raw('root_dir')->shouldReturn('/My/Custom/Dir');
+        $this->raw('debug')->shouldReturn(true);
+
+        $this->raw('root_dir');
+        $this->raw('debug');
+    }
+}
