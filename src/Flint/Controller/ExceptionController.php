ac0f3eb6077ed692296ef62b0a1d337607a19f77
Henrik Bjørnskov <henrik@bjrnskov.dk>
2013-01-10 11:26:22 +0100

    Add default production error template

--- src/Flint/Controller/ExceptionController.php
+++ src/Flint/Controller/ExceptionController.php
@@ -5,6 +5,7 @@ namespace Flint\Controller;
 use Symfony\Component\HttpKernel\Debug\ExceptionHandler;
 use Symfony\Component\HttpKernel\Exception\FlattenException;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * @package Flint
@@ -20,6 +21,48 @@ class ExceptionController extends Controller
     {
         $handler = new ExceptionHandler($this->app['debug']);
 
+        if ($this->app['debug']) {
+            return $handler->createResponse($exception);
+        }
+
+        $this->app['twig.loader.filesystem']->addPath(__DIR__ . '/../Resources/views');
+
+        $code = $exception->getStatusCode();
+        $template = $this->resolve($request, $code, $format);
+
+        if ($template) {
+            return $this->app['twig']->render($template, array(
+                'status_code'    => $code,
+                'status_text'    => isset(Response::$statusTexts[$code]) ? Response::$statusTexts[$code] : '',
+                'exception'      => $exception,
+            ));
+        }
+
         return $handler->createResponse($exception);
+    }
+
+    /**
+     * @param Request $request
+     * @param integer $code
+     * @param string $format
+     * @return string|null
+     */
+    protected function resolve(Request $request, $code, $format)
+    {
+        $loader = $this->app['twig.loader'];
+
+        $templates = array(
+            'Exception/error' . $code . '.' . $format . '.twig',
+            'Exception/error.' . $format . '.twig',
+            'Exception/error.twig',
+        );
+
+        foreach ($templates as $template) {
+            if (false == $loader->exists($template)) {
+                continue;
+            }
+
+            return $template;
+        }
     }
 }
