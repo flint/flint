3f817d0db92399a284d24615e57f5971628533c5
Henrik Bjørnskov <henrik@bjrnskov.dk>
2013-05-21 13:41:20 +0200

    remove ext

--- doc/conf.py
+++ doc/conf.py
@@ -28,7 +28,6 @@ def get_version():
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
-sys.path.append(os.path.abspath('_exts'))
 
 # -- General configuration -----------------------------------------------------
 
@@ -37,7 +36,7 @@ sys.path.append(os.path.abspath('_exts'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['configurationblock']
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
