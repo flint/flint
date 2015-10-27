ef35b4adb807369390c650efc58d881dac318314
Henrik Bjornskov <hb@peytz.dk>
2015-10-27 10:33:04 +0100

    fix get_version

--- doc/conf.py
+++ doc/conf.py
@@ -59,7 +59,7 @@ copyright = u'2013, Henrik Bjrnskov'
 # built documents.
 #
 # The short X.Y version.
-version = get_version().lstrip('v')
+version = version
 # The full version, including alpha/beta/rc tags.
 release = version
 
